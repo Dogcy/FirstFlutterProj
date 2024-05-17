1388958c7a910b13cf15a504d7e64b602a896a10
Dogcy <ccy810127@gmail.com>
2024-05-17 19:40:31 +0800

    將main改基本template

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
-import 'gradient_underscore_container.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.blueAccent,
-      body: GradientContainer(
-        Colors.black, Colors.yellow,Colors.green
-      ),
-    ),
-  ));
+  runApp(
+    const MaterialApp(),
+  );
 }
 
 // decoration 裝飾ｌ
