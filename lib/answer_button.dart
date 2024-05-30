dfe1990101e94c752205ef023b47a7e31ad8ec9c
Dogcy <ccy810127@gmail.com>
2024-05-31 01:51:32 +0800

    69完成  新增jason格式 按鈕

---
+++ lib/answer_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(this.answerText, this.onTap, {super.key});
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+          foregroundColor: const Color.fromARGB(255, 33, 1, 85),
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10)))),
+      onPressed: onTap,
+      child: Text(answerText),
+    );
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'answer_button.dart';
+
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
@@ -10,6 +12,18 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('data');
+    return SizedBox(
+      width: double.infinity, // 寬度無窮大
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('The quetsion....'),
+          SizedBox(height: 30),
+          AnswerButton('Awswer1', () {}),
+          AnswerButton('Awswer2', () {}),
+          AnswerButton('Awswer3', () {}),
+        ],
+      ),
+    );
   }
 }
