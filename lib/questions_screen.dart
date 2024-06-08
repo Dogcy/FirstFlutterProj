35ca4a31564e4a11741fc7f5da4ddc2e103b7bd6
Dogcy <ccy810127@gmail.com>
2024-06-08 15:29:32 +0800

    第三章 72節 完成

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'answer_button.dart';
+import 'package:practise_project/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,19 +11,38 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  final currentQuestion = questions[0];
+
   @override
   Widget build(BuildContext context) {
+    int index = 0;
     return SizedBox(
       width: double.infinity, // 寬度無窮大
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('The quetsion....'),
-          SizedBox(height: 30),
-          AnswerButton('Awswer1', () {}),
-          AnswerButton('Awswer2', () {}),
-          AnswerButton('Awswer3', () {}),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text.toString(),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 30),
+
+            ...currentQuestion.answers.map((item) {
+              return AnswerButton(item, () {
+                print('Index: $index, Item: $item');
+                index++;
+              });
+            }),
+            // AnswerButton('Awswer1', () {
+            //   print('test');
+            // }),
+            // AnswerButton('Awswer2', () {}),
+            // AnswerButton('Awswer3', () {}),
+          ],
+        ),
       ),
     );
   }
